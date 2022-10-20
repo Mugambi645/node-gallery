44b50be0bc3646d56a5a647e241125acb2fdf1cc
patrick mugambi <patrick@kali.kali>
2022-10-20 19:17:15 +0300

    add load more button

--- client/src/App.js
+++ client/src/App.js
@@ -5,19 +5,39 @@ import { useEffect, useState } from "react";
 import { getImages } from './api';
 function App() {
   const [imageList, setImageList] = useState([]);
+  const [nextCursor, setNextCursor] = useState(null);
   useEffect(() => {
     const fetchData = async() => {
       const resJson = await getImages();
       setImageList(resJson.resources);
+      setNextCursor(resJson.next_cursor);
     }
     fetchData();
   }, []);
+  const handleLoadMore = async () => {
+    const resJson = await getImages(nextCursor);
+    setImageList((currentImageList) => [
+      ...currentImageList,
+      ...resJson.resources,
+    ]);
+    setNextCursor(resJson.next_cursor);
+  }
   return (
+    <>
     <div className="image-grid">
     {imageList.map((image) => (
       <img src={image.url} alt={image.publid_id}></img>
     ))}
     </div>
+    <div className='footer'>
+      {
+        nextCursor && (
+          <button onClick={handleLoadMore}>Load more</button>
+        )
+      }
+
+    </div>
+    </>
   );
 }
 
--- client/src/api.js
+++ client/src/api.js
@@ -1,6 +1,12 @@
 const API_URL = process.env.REACT_APP_API_URL;
-export const getImages = async () => {
-    const res = await fetch(`${API_URL}/photos`);
+export const getImages = async (nextCursor) => {
+    const params = new URLSearchParams();
+
+    if (nextCursor) {
+        //check if more images exist
+        params.append('next_cursor', nextCursor);
+    }
+    const res = await fetch(`${API_URL}/photos?${params}`);
     const resJson = await res.json();
     return resJson;
 }
