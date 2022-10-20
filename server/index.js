6b14962189f9baf0aecab756f39572c2032a4b08
patrick mugambi <patrick@kali.kali>
2022-10-20 17:52:07 +0300

    create initial server configurations

--- server/index.js
+++ server/index.js
@@ -3,3 +3,11 @@ const dotenv = require("dotenv");
 const cors = require("cors");
 const {json} = require("body-parser");
 const axios = require("axios");
+const app = express();
+const PORT = 5000;
+app.use(cors());
+app.use(json());
+app.get("/photos", async(req,res) =>{
+    return res.send({message: "Hello"})
+})
+app.listen(PORT, () => console.log(`Server listening at port ${PORT}`))
