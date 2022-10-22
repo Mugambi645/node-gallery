ee93330277a004b08c5d9cefcad23e4bcfcf1f61
patrick mugambi <patrick@kali.kali>
2022-10-22 15:58:55 +0300

    add backend search logic

--- server/index.js
+++ server/index.js
@@ -8,14 +8,26 @@ const PORT = 5000;
 app.use(cors());
 app.use(json());
 const { parsed: config} = dotenv.config();
-const BASE_URL = `https://api.cloudinary.com/v1_1/${config.CLOUD_NAME}/resources/image`
+const BASE_URL = `https://api.cloudinary.com/v1_1/${config.CLOUD_NAME}`
 const auth = {
     username: config.API_KEY,
     password: config.API_SECRET
 }
 app.get("/photos", async(req,res) =>{
-    const response = await axios.get(BASE_URL, {
+    const response = await axios.get(BASE_URL + '/resources/image', {
         auth,
+        params: {
+            next_cursor: req.query.next_cursor,
+        }
+    })
+    return res.send(response.data)
+})
+app.get("/search", async(req,res) => {
+    const response = await axios.get(BASE_URL + '/resources/search', {
+        auth,
+        params: {
+            expression: req.query.expression,
+        }
     })
     return res.send(response.data)
 })
