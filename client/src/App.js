5517a554880f6ee43a08d1b98788607494ebe9c6
patrick mugambi <patrick@kali.kali>
2022-10-22 16:18:26 +0300

    create search form

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import { getImages } from './api';
 function App() {
   const [imageList, setImageList] = useState([]);
   const [nextCursor, setNextCursor] = useState(null);
+  const [searchValue, setSearchValue] = useState('');
   useEffect(() => {
     const fetchData = async() => {
       const resJson = await getImages();
@@ -24,6 +25,14 @@ function App() {
   }
   return (
     <>
+    <form>
+      <input
+      value={searchValue}
+      onChange={(event) => setSearchValue(event.target.value)}
+      required="required" placeholder='Enter a search value..'></input>
+      <button type='submit'>Search</button>
+      <button type='button'>Clear</button>
+    </form>
     <div className="image-grid">
     {imageList.map((image) => (
       <img src={image.url} alt={image.publid_id}></img>
